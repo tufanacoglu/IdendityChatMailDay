e2eb0f75c3f6914b3d04293cd5f1fc1a8ccfa08f
tufanacoglu <tufanacoglu27@gmail.com>
2025-04-29 15:45:04 +0300

    Add project files.

---
+++ IdendityChatMailDay/Context/MailContext.cs
@@ -0,0 +1,14 @@
+﻿using IdendityChatMailDay.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdendityChatMailDay.Context
+{
+    public class MailContext : IdentityDbContext<AppUser>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Tufan;Initial Catalog=EmailChatDb;Integrated Security=True;Trust Server Certificate=True");
+        }
+    }
+}
---
+++ IdendityChatMailDay/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using IdendityChatMailDay.Entities;
+using IdendityChatMailDay.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdendityChatMailDay.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                Email = model.Email,
+                UserName = model.Usarname
+            };
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("UserLogin", "Login");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+                return View();
+            }
+        }
+    }
+}
